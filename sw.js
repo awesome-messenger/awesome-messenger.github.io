23559a5e1a028822545bf82e518a91dd0febd35d
awesome-messenger <awesome.industries.russia@gmail.com>
2026-07-19 00:05:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Awesome Messenger — service worker: кэш файлов + клики по уведомлениям */
-var CACHE = "am-v3-4";
+var CACHE = "am-v3-5";
 var CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable.png"];
 
 self.addEventListener("install", function (e) {
